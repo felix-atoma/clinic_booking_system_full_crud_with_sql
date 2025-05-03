91fc2b05915dfbc68eec1fa4f533f58ae51cd3c9
felix-atoma <felixatoma2@gmail.com>
2025-05-03 23:42:07 +0000

    added new port

--- models/db.js
+++ models/db.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-connection.connect(err => {
-  if (err) console.error('Database connection error:', err);
-  else console.log('Connected to MySQL');
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Connected to MySQL');
+  }
 });
 
-module.exports = connection;
+module.exports = db;
